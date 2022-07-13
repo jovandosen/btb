e1557b05008ca2ab6aaec311d6443b87abd8f44b
Jovan Dosen <jovandosen1994@gmail.com>
2022-07-13 17:04:53 +0200

    Shell scripts

---
+++ helper/shell-scripts.php
@@ -0,0 +1,29 @@
+<?php
+
+// $output = null;
+// $retval = null;
+
+// exec('whoami', $output, $retval);
+// exec('ls', $output, $retval);
+// exec('which php', $output, $retval); // /usr/bin/php
+// exec('which mysql', $output, $retval);
+// exec('which tar', $output, $retval);
+// exec('/user/bin/tar czf /var/www/btb/file_backups/app.backup2.tar.gz /var/www/btb', $output, $retval);
+// exec('php -v', $output, $retval); 
+// exec('tar --version', $output, $retval); 
+// exec('tar czf ../../btb/file_backups/app.backup2.tar.gz ../../btb', $output, $retval);
+// exec('php php-info.php', $output, $retval);
+
+// $result = shell_exec('whoami');
+// $result = shell_exec('php -v');
+// $result = shell_exec('which tar');
+// $result = shell_exec('which php');
+// $result = shell_exec('which mysql');
+// $result = shell_exec('ls');
+// $result = shell_exec('php php-info.php');
+
+// $res = system('whoami', $return);
+// system('which php', $return);
+// system('which tar', $return);
+// system('which mysql', $return);
+// system('php php-info.php', $return);
